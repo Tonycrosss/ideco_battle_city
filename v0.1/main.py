66e69bab3dd98d9e98b26a20ec76a362657f0d44
TonyCross <starovoitov.anton@gmail.com>
2017-02-15 20:30:35 +0500

    v0.1

--- v0.1/main.py
+++ v0.1/main.py
@@ -1,1 +1,73 @@
 import pygame
+from pygame import *
+
+window_width = 830
+window_height = 830
+display = (window_width, window_height)
+background_color = "#004400"  # Green BG
+
+# bricks vars
+brick_width = 32
+brick_height = 32
+brick_color = "#FF6262"
+
+
+def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode(display)
+    pygame.display.set_caption("Ideco BattleCity")
+    bg = Surface((window_width, window_height))
+    bg.fill(Color(background_color))
+
+    while 1:
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                raise SystemExit
+            screen.blit(bg, (0, 0))
+
+            #  creating bricks
+            x = y = 0
+            for row in level:
+                for col in row:
+                    if col == "#":
+                        #  Create block and fill with color
+                        br = Surface((brick_width, brick_height))
+                        br.fill(Color(brick_color))
+                        screen.blit(br, (x, y))
+
+                    x += brick_width
+                y += brick_height
+                x = 0
+            pygame.display.update()
+
+if __name__ == '__main__':
+    level = [
+        "..........................",
+        "..........................",
+        "..##..##..##..##..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..##@@##..##..##..",
+        "..##..##..##@@##..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..........##..##..",
+        "..##..##..........##..##..",
+        "..........##..##..........",
+        "..........##..##..........",
+        "##..####..........####..##",
+        "@@..####..........####..@@",
+        "..........##..##..........",
+        "..........######..........",
+        "..##..##..######..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..##..##..##..##..",
+        "..##..##..........##..##..",
+        "..##..##..........##..##..",
+        "..##..##...####...##..##..",
+        "...........#..#...........",
+        "...........#..#..........."]
+
+    main()
